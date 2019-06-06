c95b964e2954a65eccee1befca7f746718777c3d
mwh <526184195@qq.com>
2019-06-06 16:21:01 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios"
+// element: 1.导入组件
+import ElementUI from "element-ui"
+// element的样式
+import 'element-ui/lib/theme-chalk/index.css';
+import VueRouter from "vue-router"
 
-Vue.config.productionTip = false
+// element: 2.注册element插件
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+const routes=[
+
+];
+
+const router = new VueRouter({routes})
+
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
